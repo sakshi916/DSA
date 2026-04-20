1fcb580bb7d99f9a8b4b8d0f08d804932069b206
Sakshi Lunawat <56351703+sakshi916@users.noreply.github.com>
2026-04-20 11:54:34 +0530

    Create MinCostForTrainTickets.java

---
+++ dp/medium/MinCostForTrainTickets.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/minimum-cost-for-tickets/
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int lastDay =  days[days.length - 1];
+        int[] dp = new int[lastDay+1];
+        boolean[] isTravelDay = new boolean[lastDay+1];
+        for(int day : days)isTravelDay[day] = true;
+
+        for(int i=1;i<=lastDay;i++){
+            if(!isTravelDay[i]){
+                dp[i]=dp[i-1];
+                continue;
+            }
+            dp[i]=costs[0] + dp[i-1];
+            dp[i]=Math.min(costs[1] + dp[Math.max(i-7,0)],dp[i]);
+            dp[i] = Math.min(costs[2] + dp[Math.max(i-30,0)],dp[i]);
+        }
+        return dp[lastDay];
+    }
+}
