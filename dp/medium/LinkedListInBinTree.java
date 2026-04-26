f868037162c4362865435d7b20a54b18779aaa4e
Sakshi Lunawat <56351703+sakshi916@users.noreply.github.com>
2026-04-26 20:43:25 +0530

    Create LinkedListInBinTree.java

---
+++ dp/medium/LinkedListInBinTree.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/linked-list-in-binary-tree/
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) return false;
+        if (checkPath(head, root)) return true;
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean checkPath(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null || head.val != root.val) return false;
+        return checkPath(head.next, root.left) || checkPath(head.next, root.right);
+    }
+}
